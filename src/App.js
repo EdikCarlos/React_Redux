a8670bbb5a1fa16ad71bf95d7dcb2bcdd83c24d5
EdikCarlos <edikcarlos@hotmail.com>
2021-01-19 23:56:22 -0300

    Projeto de React com Redux, temos 2 reducers fazendo actions diferentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 import './App.css';
 
+
 function App() {
+
+  const state = useSelector((state) => state.reducer);
+  const nome = useSelector((state) => state.reducer2);
+  const dispatch = useDispatch();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="box1">
+        <h1 className="estado">
+          {state}
+        </h1>
+        <button className="btn1" onClick={() => dispatch({ type: 'SOMAR' })}>
+          Somar +1
+        </button>
+        <button className="btn2" onClick={() => dispatch({ type: 'SUBTRAIR' })}>
+          Subtrair -1
+        </button>
+      </div>
+      <div className="box2">
+        <h1>
+          {nome}
+        </h1>
+        <button className='red' onClick={() => dispatch({ type: 'RED'})}>
+        </button>
+        <button className='green' onClick={() => dispatch({ type: 'GREEN'})}>
+        </button>
+        <button className='yellow' onClick={() => dispatch({ type: 'YELLOW'})}>
+        </button>
+        <button className='pink' onClick={() => dispatch({ type: 'PINK'})}>
+        </button>
+        <button className='black' onClick={() => dispatch({ type: 'BLACK'})}>
+        </button>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers} from 'redux';
+import { Provider } from 'react-redux';
+
+function reducer(state = 0, action) {
+  if(action.type === 'SOMAR') {
+    return state + 1;
+  }
+  else if(action.type === 'SUBTRAIR') {
+    return state - 1;
+  }
+  else {
+    return state;
+  }
+}
+
+function reducer2(state = 'CLIQUE NA COR ABAIXO PARA SABER SEU NOME EM INGLÊS', action) {
+  if(action.type === 'RED') {
+    return state = 'RED';
+  }
+  else if(action.type === 'GREEN') {
+    return state = 'GREEN';
+  } 
+  else if(action.type === 'YELLOW') {
+    return state = 'YELLOW';
+  }
+  else if(action.type === 'PINK') {
+    return state = 'PINK';
+  }
+  else if(action.type === 'BLACK') {
+    return state = 'BLACK';
+  }
+  else {
+    return state;
+  }
+}
+
+const reducers = combineReducers( {reducer, reducer2} );
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
